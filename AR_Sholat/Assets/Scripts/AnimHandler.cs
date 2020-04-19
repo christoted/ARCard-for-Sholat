0b80c0992155fd384a393642c5f0531347457ffd
Luis Anthonie Alkins <46640013+luisanthoniealkins@users.noreply.github.com>
2020-04-19 21:31:04 +0700

    Revise Script

--- AR_Sholat/Assets/Scripts/AnimHandler.cs
+++ AR_Sholat/Assets/Scripts/AnimHandler.cs
@@ -6,50 +6,64 @@ using Vuforia;
 
 public class AnimHandler : MonoBehaviour, ITrackableEventHandler
 {
-   // public Text debugText;
+    public Text debugText;
     
     [SerializeField] private AnimationClip[] animations;
     [SerializeField] private int[] frameStart, frameEnd;
     [SerializeField] private AudioClip[] audios;    
     [SerializeField] private int[] loopingAudio;
+    private Button buttonPlay, buttonPause;
 
     private int curAnim = 0, numLoop;
-    private bool isTracked = false, isFinished;
-    private float curNextAnim;
+    private bool isTracked = false, isFinished, isPaused = false;
+    private float NextAnimCD, NextSameAnimCD;
     private Animator anim;
     private AudioSource audioSource;
     protected TrackableBehaviour trackableBehavior;
+    
     void Start()
     {
         anim = transform.GetComponent<Animator>();    
         audioSource = transform.GetComponent<AudioSource>();
         trackableBehavior = GetComponentInParent<TrackableBehaviour>();
         if (trackableBehavior) trackableBehavior.RegisterTrackableEventHandler(this);                    
-    }
+
+        GameObject.Find("PlayButton").GetComponent<Button>().onClick.AddListener(Play);
+        GameObject.Find("PauseButton").GetComponent<Button>().onClick.AddListener(Pause);
+        
+        // GameObject.Find("/Canvas/Play").GetComponent<Button>().onClick.AddListener(Play);
+        // GameObject.Find("/Canvas/Pause").GetComponent<Button>().onClick.AddListener(Pause);
+        // GameObject.Find("Play").GetComponent<Button>().onClick.AddListener(Play);
+        // buttonPause.GetComponent<Button>().onClick.AddListener(Pause);
+    }   
 
     
     void Update()
     {
         if(!isTracked) return;
-                
-        // debugText.text = Time.time + " " + nextAnim;
+        if (isPaused) return;    
 
         if (loopingAudio[curAnim] == 0){
             //AGAR DIA HANYA DI RUN SEKALI
-            if (isFinished) return;            
+            
+
+            if (isFinished) {
+                NextAnimCD -= Time.deltaTime;
+                if (NextAnimCD < 0) PlayNextAnim();
+                return;
+            }
             isFinished = true;
             
             anim.Play(animations[curAnim].name, 0, getNT(curAnim));            
             audioSource.clip = audios[curAnim];
             audioSource.Play();
-            
-            
-            Invoke("PlayNextAnim",((float)frameEnd[curAnim]-frameStart[curAnim]) / 25);
+
+            NextAnimCD = ((float)frameEnd[curAnim]-frameStart[curAnim]) / 25;            
         } else {
             //LOOPING SELAMA AUDIO JALAN X KALI
             if (!audioSource.isPlaying){
                 if (numLoop == 0){
-                    Invoke("PlayNextAnim",0);                    
+                    PlayNextAnim();
                 } else {
                     numLoop--;
                     audioSource.clip = audios[curAnim];
@@ -57,18 +71,21 @@ public class AnimHandler : MonoBehaviour, ITrackableEventHandler
                 }
             } 
             // ANIMASI LOOPING SAMPAI AUDIO SELESAI
-            if (curNextAnim < Time.time){                
-                curNextAnim = Time.time + (((float)frameEnd[curAnim]-frameStart[curAnim]) / 25);
+            NextSameAnimCD -= Time.deltaTime;
+            if (NextAnimCD < 0){                                
                 anim.Play(animations[curAnim].name, 0, getNT(curAnim));
+                NextSameAnimCD = (((float)frameEnd[curAnim]-frameStart[curAnim]) / 25);
             }
 
         }
     }
 
     private void PlayNextAnim(){
-        curAnim = (curAnim+1) % animations.Length;
-        numLoop = loopingAudio[curAnim];
-        curNextAnim = Time.time;
+        curAnim++;
+        debugText.text = curAnim + "";
+        if (curAnim < animations.Length) numLoop = loopingAudio[curAnim];
+        else Pause();
+        NextSameAnimCD = 0;
         isFinished = false;
         audioSource.Stop();
     }
@@ -90,8 +107,12 @@ public class AnimHandler : MonoBehaviour, ITrackableEventHandler
     }
     private void OnTrackingFound(){
         curAnim = 0;
+        numLoop = loopingAudio[curAnim];
         isTracked = true;
-        curNextAnim = Time.time;
+        isPaused = false;
+        isFinished = false;
+        
+        NextAnimCD = 0;
     }
 
     private void OnTrackingLost(){
@@ -110,5 +131,25 @@ public class AnimHandler : MonoBehaviour, ITrackableEventHandler
     private float getNT(int id){
         int totalFrame = getFrame(animations[id]);
         return (float)frameStart[id]/totalFrame;
+    }
+
+    private void Play(){
+        if (!isPaused) return;
+        isPaused = false;
+        if (curAnim == animations.Length) {
+            curAnim = 0;
+            numLoop = loopingAudio[curAnim];
+        }
+        audioSource.Play();        
+        debugText.text = "Play";
+        anim.speed = 1;
+    }
+
+    private void Pause(){
+        if (isPaused) return;
+        isPaused = true;
+        audioSource.Pause();
+        debugText.text = "Pause";
+        anim.speed = 0;
     }
 }
